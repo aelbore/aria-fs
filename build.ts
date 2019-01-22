0c6ba652fc8b9b1dd12ce1e3de85328919277e86
aelbore <aelbore.sg@gmail.com>
2019-01-23 07:26:57 +0800

    add dependencies, add build

--- build.ts
+++ build.ts
@@ -5,10 +5,13 @@ import * as fs from 'fs';
 import { rollup } from 'rollup';
 import { clean, mkdirp } from './src/file';
 
+import MagicString from 'magic-string';
+
 const typescript2 = require('rollup-plugin-typescript2');
 const resolve = require('rollup-plugin-node-resolve');
 
 const copyFileAsync = util.promisify(fs.copyFile)
+const writeFileAsync = util.promisify(fs.writeFile)
 
 const rollupConfig = {
   inputOptions: {
@@ -18,13 +21,18 @@ const rollupConfig = {
     plugins: [
       typescript2({
         tsconfigDefaults: { 
-          compilerOptions: { target: 'es6', module: 'esNext' } 
+          compilerOptions: { 
+            target: 'es6', 
+            module: 'es2015', 
+            declaration: true 
+          } 
         },
         check: false,
         cacheRoot: path.join(path.resolve(), 'node_modules/.tmp/.rts2_cache'), 
-        useTsconfigDeclarationDir: true
+        useTsconfigDeclarationDir: false
       }),
-      resolve()
+      resolve(),
+      stripCode()
     ],
     onwarn (warning) {
       if (warning.code === 'THIS_IS_UNDEFINED') { return; }
@@ -40,17 +48,36 @@ const rollupConfig = {
   }
 }
 
+function stripCode () {
+  return {
+    name: 'stripCode',
+    transform (source, id) {
+      let code = source.replace(/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g, '')
+      const magicString = new MagicString(code)
+      let map = magicString.generateMap({hires: true})
+      return {code, map}
+    }
+  }
+}
+
+function copyPackageFile() {
+  const FILE_NAME = 'package.json';
+  const pkg = require(`./${FILE_NAME}`);
+  delete pkg.scripts;
+  delete pkg.devDependencies;
+  delete pkg.nyc;
+  return writeFileAsync(`dist/${FILE_NAME}`, JSON.stringify(pkg, null, 2))
+}
+
 function rollupBuild({ inputOptions, outputOptions }): Promise<any> {
   return rollup(inputOptions).then(bundle => bundle.write(outputOptions));
 }
 
-const miscFiles = [ 'package.json', 'README.md' ]
-
-clean('dist')
-  .then(() => {
-    mkdirp('dist')
-    return Promise.all([ 
-      rollupBuild(rollupConfig),  
-      Promise.all(miscFiles.map(file => copyFileAsync(file, `dist/${file}`)))
-    ])
-  })
+clean('dist').then(() => {
+  mkdirp('dist')
+  return Promise.all([ 
+    copyPackageFile(), 
+    rollupBuild(rollupConfig), 
+    copyFileAsync('README.md', 'dist/README.md') 
+  ])
+})
