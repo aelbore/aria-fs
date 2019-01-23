3225c5381905f6bf07e04ddd8d7eee4b4a5af27e
aelbore <aelbore.sg@gmail.com>
2019-01-23 20:21:49 +0800

    add build for dts file

--- build.ts
+++ build.ts
@@ -12,6 +12,7 @@ const resolve = require('rollup-plugin-node-resolve');
 
 const copyFileAsync = util.promisify(fs.copyFile)
 const writeFileAsync = util.promisify(fs.writeFile)
+const renameAsync = util.promisify(fs.rename);
 
 const rollupConfig = {
   inputOptions: {
@@ -24,8 +25,9 @@ const rollupConfig = {
           compilerOptions: { 
             target: 'es6', 
             module: 'es2015', 
-            declaration: true 
-          } 
+            declaration: true
+          },
+          include: [ 'src/file.ts' ]
         },
         check: false,
         cacheRoot: path.join(path.resolve(), 'node_modules/.tmp/.rts2_cache'), 
@@ -60,24 +62,24 @@ function stripCode () {
   }
 }
 
-function copyPackageFile() {
+async function copyPackageFile() {
   const FILE_NAME = 'package.json';
   const pkg = require(`./${FILE_NAME}`);
   delete pkg.scripts;
   delete pkg.devDependencies;
-  delete pkg.nyc;
   return writeFileAsync(`dist/${FILE_NAME}`, JSON.stringify(pkg, null, 2))
 }
 
-function rollupBuild({ inputOptions, outputOptions }): Promise<any> {
+async function rollupBuild({ inputOptions, outputOptions }): Promise<any> {
   return rollup(inputOptions).then(bundle => bundle.write(outputOptions));
 }
 
-clean('dist').then(() => {
-  mkdirp('dist')
-  return Promise.all([ 
-    copyPackageFile(), 
-    rollupBuild(rollupConfig), 
-    copyFileAsync('README.md', 'dist/README.md') 
-  ])
+clean('dist')
+  .then(() => rollupBuild(rollupConfig))
+  .then(() => {
+    return Promise.all([ 
+      copyPackageFile(), 
+      renameAsync('dist/file.d.ts', 'dist/aria-fs.d.ts'),
+      copyFileAsync('README.md', 'dist/README.md'),
+    ])
 })
