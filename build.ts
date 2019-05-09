3abc9721b47947ee978cdc9daf27cca402754030
aelbore <aelbore.sg@gmail.com>
2019-05-09 10:19:19 +0100

    make aria-build as devDependencies

--- build.ts
+++ build.ts
@@ -1,99 +1,34 @@
-import * as path from 'path';
-import * as util from 'util';
-import * as fs from 'fs';
+import { bundle, clean } from 'aria-build'
 
-import { rollup } from 'rollup';
-import { clean } from './src/file';
-
-import MagicString from 'magic-string';
-
-const typescript2 = require('rollup-plugin-typescript2');
-const resolve = require('rollup-plugin-node-resolve');
-
-const copyFileAsync = util.promisify(fs.copyFile)
-const writeFileAsync = util.promisify(fs.writeFile)
-const renameAsync = util.promisify(fs.rename);
-
-const DEST_FOLDER = 'dist';
-const ENTRY_FILE = 'src/file.ts'
-const MODULE_NAME = 'aria-fs';
-
-const createConfig = () => {
-  return [ 'cjs', 'es' ].map(format => {
-    const file = (format === 'es') 
-      ? path.join(DEST_FOLDER, `${MODULE_NAME}.es.js`)
-      : path.join(DEST_FOLDER, `${MODULE_NAME}.js`)
-
-    return {
-      inputOptions: {
-        treeshake: true,
-        input: ENTRY_FILE,
-        external: [ 'fs', 'util', 'path', 'minimatch' ],
-        plugins: [
-          typescript2({
-            tsconfigDefaults: { 
-              compilerOptions: { 
-                target: (format === 'cjs') ? 'es6': 'esNext', 
-                module: 'es2015', 
-                declaration: true,
-                noEmitHelpers: false
-              },
-              include: [ ENTRY_FILE ]
-            },
-            check: false,
-            cacheRoot: path.join(path.resolve(), 'node_modules/.tmp/.rts2_cache'), 
-            useTsconfigDeclarationDir: (format === 'es') ? true: false
-          }),
-          resolve(),
-          stripCode()
-        ],
-        onwarn (warning) {
-          if (warning.code === 'THIS_IS_UNDEFINED') { return; }
-          console.log("Rollup warning: ", warning.message);
-        }
+(async function() {
+  const external = [
+    'minimatch'
+  ]
+  
+  const options = [
+    {
+      input: './src/file.ts',
+      external,
+      output: {
+        format: 'es',
+        file: './dist/aria-fs.es.js'
       },
-      outputOptions: {
-        sourcemap: false,
-        exports: 'named',
-        file: file,
-        name: MODULE_NAME, 
-        format: format
+      tsconfig: {
+        compilerOptions: {
+          declaration: true
+        }
       }
-    }    
-  })
-}
-
-function stripCode () {
-  return {
-    name: 'stripCode',
-    transform (source, id) {
-      let code = source.replace(/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*+\/)|(\/\/.*)/g, '')
-      const magicString = new MagicString(code)
-      let map = magicString.generateMap({hires: true})
-      return {code, map}
+    },
+    {
+      input: './src/file.ts',
+      external,
+      output: {
+        format: 'cjs',
+        file: './dist/aria-fs.js'
+      }
     }
-  }
-}
+  ]
 
-async function copyPackageFile() {
-  const FILE_NAME = 'package.json';
-  const pkg = require(`./${FILE_NAME}`);
-  delete pkg.scripts;
-  delete pkg.devDependencies;
-  delete pkg.nyc;
-  return writeFileAsync(`dist/${FILE_NAME}`, JSON.stringify(pkg, null, 2))
-}
-
-async function rollupBuild({ inputOptions, outputOptions }): Promise<any> {
-  return rollup(inputOptions).then(bundle => bundle.write(outputOptions));
-}
-
-clean('dist')
-  .then(() => Promise.all(createConfig().map(config => rollupBuild(config))))
-  .then(() => {
-    return Promise.all([ 
-      copyPackageFile(), 
-      renameAsync('dist/file.d.ts', 'dist/aria-fs.d.ts'),
-      copyFileAsync('README.md', 'dist/README.md'),
-    ])
-})
+  await clean('dist')
+  await bundle(options)
+})()
